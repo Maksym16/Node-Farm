25e680973d5ed8e0b7665c7d4f83377bef567d05
Maksym <mfedorenko16@gmail.com>
2019-11-17 22:42:08 -0500

    added package json, nodemon, slugify

--- starter/index.js
+++ starter/index.js
@@ -1,56 +1,22 @@
 const fs = require('fs'); //build in module which allowes u to fetch data from file System
 const http = require('http');
 const url = require('url');
-
-/////////////////////////////////
-//reading and writing from the file
-
-// //Blocking, Synchros
-// const myText = fs.readFileSync('./txt/input.txt', 'utf-8'); //to redd from the file
-// const textOut = `This is what we know about the avocado: ${myText}`;
-// fs.writeFileSync('./txt/output.txt', textOut); //to write to the file
-
-// //Non-blocking, Async
-// // eslint-disable-next-line handle-callback-err
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//     console.log(data2);
-//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//         console.log('your file has been written');
-//       });
-//     });
-//   });
-// });
-// console.log('bla bla bla');
+const replaceTemp = require('./modules/replaceTemp');
 
 //////////////////////////////////
 //Server
 //readFilesSync runs only one time when we start a program
-const replaceTemp = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/templete-overview.html`,
   'utf-8'
 );
+
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/templete-card.html`,
   'utf-8'
 );
+
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/templete-product.html`,
   'utf-8'
